9ed535815c20fb40d7399d02ece511936a95efc2
Arinasse-alakhir <fatimaezzahraalakhir21@gmail.com>
2025-12-15 12:47:52 +0100

    files modified providers versins variables and main

---
+++ Infra/Terraform/main.tf
@@ -0,0 +1,34 @@
+module "s3_tfstate" {
+  source       = "./modules/s3-tfstate"
+  project_name = var.project_name
+  aws_region   = var.aws_region
+}
+
+module "vpc" {
+  source       = "./modules/vpc"
+  project_name = var.project_name
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+}
+
+module "ecr" {
+  source       = "./modules/ecr"
+  project_name = var.project_name
+}
+
+module "iam" {
+  source       = "./modules/iam"
+  project_name = var.project_name
+}
+
+module "jenkins_ec2" {
+  source       = "./modules/jenkins-ec2"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_id    = module.vpc.public_subnets[0]
+}
