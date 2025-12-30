1ae526165e02024db85a868a042171e0c90f0e2b
kamiliaAhram <kameliaahram2@gmail.com>
2025-12-30 16:25:54 +0100

    ip_output

--- Infra/Terraform/modules/jenkins-ec2/outputs.tf
+++ Infra/Terraform/modules/jenkins-ec2/outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
   description = "L'adresse IP publique du serveur Jenkins"
-  value       = aws_instance.jenkins.public_ip
+  value       = aws_eip.instance_eip.public_ip
 }
 
 output "instance_id" {
