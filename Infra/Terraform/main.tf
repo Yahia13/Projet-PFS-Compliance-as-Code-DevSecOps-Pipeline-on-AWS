e0d53348557239658ce7f1d3ce18c8ecfa319b69
yahia <yahia.ettalibi13.ye@gmail.com>
2025-12-31 10:35:43 +0100

    hh

--- Infra/Terraform/main.tf
+++ Infra/Terraform/main.tf
@@ -39,7 +39,7 @@ module "ansible_manager" {
   source       = "./modules/ansible-manager"
   project_name = var.project_name
   vpc_id       = module.vpc.vpc_id
-  subnet_id    = module.vpc.public_subnets[0]
+  subnet_id    = module.vpc.public_subnet_ids[0]
   security_group_ids = [aws_security_group.ansible_sg.id]
 
 }
