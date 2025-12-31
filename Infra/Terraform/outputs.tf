c861c2dd7d802bdd4358a4306093d0b336c1d732
yahia <yahia.ettalibi13.ye@gmail.com>
2025-12-31 10:15:20 +0100

    t7imira

--- Infra/Terraform/outputs.tf
+++ Infra/Terraform/outputs.tf
@@ -7,3 +7,26 @@ output "jenkins_public_ip" {
 output "jenkins_private_ip" {
   value = aws_instance.jenkins.private_ip
 }
+
+# -------------------------
+# Security Group outputs
+# -------------------------
+output "jenkins_sg_id" {
+  value       = aws_security_group.jenkins_sg.id
+  description = "Security Group ID for Jenkins EC2"
+}
+
+output "ansible_sg_id" {
+  value       = aws_security_group.ansible_sg.id
+  description = "Security Group ID for Ansible Manager EC2"
+}
+
+output "eks_cluster_sg_id" {
+  value       = aws_security_group.eks_cluster_sg.id
+  description = "Security Group ID for EKS control plane"
+}
+
+output "eks_nodes_sg_id" {
+  value       = aws_security_group.eks_nodes_sg.id
+  description = "Security Group ID for EKS worker nodes"
+}
