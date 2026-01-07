13e365b8ecdcb817ce4b0bca1b6abbb7b2a66147
yahia <yahia.ettalibi13.ye@gmail.com>
2026-01-07 16:59:18 +0100

    hy

--- Infra/Terraform/modules/eks/main.tf
+++ Infra/Terraform/modules/eks/main.tf
@@ -16,9 +16,9 @@ resource "aws_eks_cluster" "this" {
   }
 
   # ✅ ensure Terraform deletes node groups first[it ensures that the cluster is deleted after the node groups]
-  depends_on = [
-    aws_eks_node_group.main
-  ]
+  #depends_on = [
+   # aws_eks_node_group.main
+  #]
 }
 
 # 2. Le Node Group (Les "Muscles" - Les serveurs de travail)
