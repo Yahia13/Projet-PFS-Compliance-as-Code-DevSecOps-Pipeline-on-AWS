f5e6f0621ecb82ef5250f9607fe9f19e129398fb
yahia <yahia.ettalibi13.ye@gmail.com>
2026-01-07 19:16:50 +0100

    small changes to app

--- app/microservice/src/app.py
+++ app/microservice/src/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}, 200
+
 @app.route('/')
 def hello():
     return "Hello from my AWS DevOps Project! Deployed automatically with Bash. Built by Hardik 🚀"
