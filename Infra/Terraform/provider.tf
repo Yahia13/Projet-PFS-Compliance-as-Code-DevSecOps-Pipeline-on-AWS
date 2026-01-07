a365347cd62c4862a64d55a2a0749448649bb3a6
yahia <yahia.ettalibi13.ye@gmail.com>
2026-01-07 14:01:02 +0100

    adding the fix of the aws auth of cluster eks

--- Infra/Terraform/provider.tf
+++ Infra/Terraform/provider.tf
@@ -9,16 +9,13 @@ provider "aws" {
   }
 }
 
-data "aws_eks_cluster" "this" {
-  name = "${var.project_name}-cluster"
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = data.aws_eks_cluster.this.name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--region", var.aws_region, "--cluster-name", module.eks.cluster_name]
+  }
 }
