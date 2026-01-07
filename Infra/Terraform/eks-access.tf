721c80d4efe3cb6e2f2ef537daf1df55aa308476
yahia <yahia.ettalibi13.ye@gmail.com>
2026-01-07 18:17:38 +0100

    an other little fix

--- Infra/Terraform/eks-access.tf
+++ Infra/Terraform/eks-access.tf
@@ -13,3 +13,19 @@ resource "aws_eks_access_policy_association" "jenkins_admin" {
     type = "cluster"
   }
 }
+
+resource "aws_eks_access_entry" "console_admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = "arn:aws:iam::352324843147:user/yahia"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "console_admin_policy" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_eks_access_entry.console_admin.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
